397d6510c073f87a683874598524dd2255e29ee2
sairahul5 <sairahuladabala123@gmail.com>
2026-09-11 20:34:21 +0530

    Allow deployed frontend websocket origins

--- src/main/java/com/chitram/websocket/WebSocketConfig.java
+++ src/main/java/com/chitram/websocket/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.chitram.websocket;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -9,6 +10,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Value("${FRONTEND_URL:http://localhost:3000}")
+    private String frontendUrl;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
@@ -21,7 +25,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:3000")
+            .setAllowedOriginPatterns(frontendUrl.split(","))
                 .withSockJS();
     }
 }
